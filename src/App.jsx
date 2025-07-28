b84e263a22dde9a99b112366c1a52006d49f9fcd
nadunhasalanka <nadun202327@gmail.com>
2025-07-28 17:28:00 +0530

    All the routing after the work.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Header from './components/Header';
+import ProfileCard from './components/ProfileCard';
+import ProjectsSection from './components/ProjectsSection';
+import SkillsSection from './components/SkillsSection';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
+import FloatingIcons from './components/FloatingIcons';
+
+function App() {
+  return (
+    <>
+      <FloatingIcons />
+      <Header />
+      
+      <main className="min-h-screen" style={{paddingTop: '90px'}}>
+        {/* Hero Section / Profile */}
+        <div id="profile" className="min-h-screen flex items-center justify-center">
+          {/* <div className="max-w-6xl mx-auto px-8"> */}
+          <div className="max-w-5xl mx-auto px-8">
+
+            <ProfileCard />
+          </div>
+        </div>
+        
+        {/* Projects Section */}
+        <ProjectsSection />
+        
+        {/* Skills Section */}
+        <SkillsSection />
+        
+        {/* Contact Section */}
+        <ContactSection />
+      </main>
+      
+      <Footer />
+    </>
+  );
+}
+
+export default App;
